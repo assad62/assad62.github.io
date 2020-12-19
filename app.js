e9600c4e4459cd750e182a6e5d5f5b3a88321fe1
assad62 <assad62@hotmail.com>
2020-12-19 17:27:13 +0800

    added cors proxy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
     data(){
         return{
            
-            url: 'https://newsapi.org/v2/top-headlines?country=hk&apiKey=a5ecf0100b0149e6a1406584949890a8&pagesize=20/',
+            url: 'https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=hk&apiKey=a5ecf0100b0149e6a1406584949890a8&pagesize=20/',
             articles: [],
             articlesCount: []
            
