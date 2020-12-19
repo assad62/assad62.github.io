06d398bd986f4b5a5ee7e65caaa99fa0aca4a82f
Mohammad Assad <assad@appit.com.hk>
2020-12-19 17:05:14 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+    //data functions
+    //template: '<h2>I am the template</h2>'
+    data(){
+        return{
+           
+            url: 'http://newsapi.org/v2/top-headlines?country=hk&apiKey=a5ecf0100b0149e6a1406584949890a8&pagesize=20',
+            articles: [],
+            articlesCount: []
+           
+        }
+   
+    },
+    mounted(){
+
+        axios.
+        get(this.url)
+        .then( (response) => {{
+                this.articles = response.data.articles
+               // this.articlesCount = this.articles
+                 
+              }
+
+        })
+     
+
+      },
+    
+})
+
+app.mount('#container')
